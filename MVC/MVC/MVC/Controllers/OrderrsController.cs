7efaaa9136d1b2912dd85e77284e6ff19425ab40
nguyentuananhh3009@gmail.com <nguyentuananhh3009@gmail.com>
2022-11-19 00:58:43 +0700

    first commit

---
+++ MVC/MVC/MVC/Controllers/OrderrsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class OrderrsController : Controller
+    {
+        private DevConn db = new DevConn();
+
+        // GET: Orderrs
+        public ActionResult Index()
+        {
+            return View(db.Orderrs.ToList());
+        }
+
+        // GET: Orderrs/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Orderr orderr = db.Orderrs.Find(id);
+            if (orderr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderr);
+        }
+
+        // GET: Orderrs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orderrs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrderID,OderDate,AgentID")] Orderr orderr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orderrs.Add(orderr);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(orderr);
+        }
+
+        // GET: Orderrs/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Orderr orderr = db.Orderrs.Find(id);
+            if (orderr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderr);
+        }
+
+        // POST: Orderrs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrderID,OderDate,AgentID")] Orderr orderr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderr).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(orderr);
+        }
+
+        // GET: Orderrs/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Orderr orderr = db.Orderrs.Find(id);
+            if (orderr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderr);
+        }
+
+        // POST: Orderrs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            Orderr orderr = db.Orderrs.Find(id);
+            db.Orderrs.Remove(orderr);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC/MVC/MVC/Controllers/tblItemsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class tblItemsController : Controller
+    {
+        private DevConn db = new DevConn();
+
+        // GET: tblItems
+        public ActionResult Index()
+        {
+            return View(db.tblItems.ToList());
+        }
+
+        // GET: tblItems/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblItem tblItem = db.tblItems.Find(id);
+            if (tblItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblItem);
+        }
+
+        // GET: tblItems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,ItemName,Size")] tblItem tblItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblItems.Add(tblItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblItem);
+        }
+
+        // GET: tblItems/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblItem tblItem = db.tblItems.Find(id);
+            if (tblItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblItem);
+        }
+
+        // POST: tblItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,ItemName,Size")] tblItem tblItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblItem);
+        }
+
+        // GET: tblItems/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblItem tblItem = db.tblItems.Find(id);
+            if (tblItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblItem);
+        }
+
+        // POST: tblItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            tblItem tblItem = db.tblItems.Find(id);
+            db.tblItems.Remove(tblItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
